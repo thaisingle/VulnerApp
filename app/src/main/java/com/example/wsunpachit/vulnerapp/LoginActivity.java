9ada7e83894304661cb9ec53129cb23e25c67fa6
ITselectlab <thaisingle@hotmail.com>
2016-10-02 11:38:12 +0700

    Add Vulnerability of insecure comment (Backdoor) Add Vulnerability of bypassing the authentication Add Vulnerability of bypassing the emulator detection

--- app/src/main/java/com/example/wsunpachit/vulnerapp/LoginActivity.java
+++ app/src/main/java/com/example/wsunpachit/vulnerapp/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.wsunpachit.vulnerapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Build;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.app.AlertDialog;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,16 +21,33 @@ public class LoginActivity extends AppCompatActivity {
 
         txtUser = (EditText)findViewById(R.id.txtLogin2);
         txtPass = (EditText)findViewById(R.id.txtLogin3);
+        final Button cLogin = (Button) findViewById(R.id.cmdLogin);
+        final Button cRegister = (Button) findViewById(R.id.cmdRegister);
 
-        final Button btn1 = (Button) findViewById(R.id.cmdLogin);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        //True is Protected
+        if (isEmulator()){
+            builder.setTitle("Warning!!!");
+            builder.setMessage("App installed in Emulator do not allow the user registration");
+            builder.setPositiveButton("OK", null);
+            AlertDialog dialog = builder.show();
+
+            //hind Register button
+            cRegister.setVisibility(View.INVISIBLE);
+        }
+
+
+        //True is Pass
+        cLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
 
                 if (isLogin(txtUser.getText().toString() , txtPass.getText().toString())) {
                     Intent i = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(i);
                 }
+
             }
         });
 
@@ -40,5 +59,24 @@ public class LoginActivity extends AppCompatActivity {
         String str1 = "admin";
         String str2 = "password";
         return (paramString1.contentEquals(str1)) && (paramString2.contentEquals(str2));
+    }
+
+    //public, protected, and static could be bypassed by Mobile substrate
+    //private could not be bypassed by Mobile substrate
+    private boolean isEmulator() {
+        String strDevice = Build.FINGERPRINT;
+        return  strDevice.indexOf("vbox86p") >= 0
+                || Build.BRAND.equals("generic")
+                || Build.BRAND.equals("generic_x86");
+}
+
+    public static String getDeviceListing() {
+        return "Build.PRODUCT: " + Build.PRODUCT + "\n" +
+                "Build.MANUFACTURER: " + Build.MANUFACTURER + "\n" +
+                "Build.BRAND: " + Build.BRAND + "\n" +
+                "Build.DEVICE: " + Build.DEVICE + "\n" +
+                "Build.MODEL: " + Build.MODEL + "\n" +
+                "Build.HARDWARE: " + Build.HARDWARE + "\n" +
+                "Build.FINGERPRINT: " + Build.FINGERPRINT;
     }
 }
